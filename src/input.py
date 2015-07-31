398ea53c5bcccee761ee8f6adc97c85fb0737bdd
HugoViala <hugo.viala@gmail.com>
2015-07-30 19:23:36 -0800

    restructring the UserInput class

--- src/input.py
+++ src/input.py
@@ -1,22 +1,26 @@
 import pygame
 
+class UserInput:
+    """ store the input entered by the user for one frame """
+    def __init__(self):
+        self.move_up = False
+        self.move_left = False
+        self.move_down = False
+        self.move_up = False
+        self.action = False
+        self.use_controller = False
+    pass
 def processInput():
-    """ This function returns a dict containing the moves
-    available for the input and if they were activated
-    during the current frame """
-    user_input = {}
-    user_input["MoveUp"] = False
-    user_input["MoveDown"] = False
-    user_input["MoveLeft"] = False
-    user_input["MoveRight"] = False
+    user_input = UserInput()
+    print user_input.move_up
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                user_input["MoveLeft"] = True
+                user_input.move_left = True
             if event.key == pygame.K_UP:
-                user_input["MoveUp"] = True
+                user_input.move_up = True
             if event.key == pygame.K_RIGHT:
-                user_input["MoveRight"] = True
+                user_input.move_right = True
             if event.key == pygame.K_DOWN:
-                user_input["MoveDown"] = True
+                user_input.move_down = True
     return user_input
--- src/pkman.py
+++ src/pkman.py
@@ -1,16 +1,21 @@
 import pygame
+import input
 
 if __name__ == "__main__":
+
     pygame.init()
     clock = pygame.time.Clock()
     window_size = (700, 500)
     screen = pygame.display.set_mode(window_size)
     pygame.display.set_caption("PKMAN")
     game_running = True
+    user_input = input.UserInput()
     while game_running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 game_running = False
+            else:
+                user_input.processEvent(event)
         screen.fill((255, 255, 255))
         pygame.display.flip()
         clock.tick(60)
