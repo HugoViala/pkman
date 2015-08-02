a78db3d69404c7bc550b0a77e290e7d4ade84a17
HugoViala <hugo.viala@gmail.com>
2015-08-01 19:13:26 -0800

    player can move in several directions

--- src/game.py
+++ src/game.py
@@ -13,17 +13,16 @@ class Player:
 def updateAndRender(user_input, window_surface,
                     player):
     """ update the game and render the current frame """
-    # TODO(hugo): improve it so that we can enter both direction
     # TODO(hugo): maybe consider acceleration and equations of motion
     player.dp = pkmath.v2(0, 0)
     if user_input.move_up:
-        player.dp = pkmath.v2(0, -50)
+        player.dp = pkmath.add(player.dp, pkmath.v2(0, -50))
     if user_input.move_down:
-        player.dp = pkmath.v2(0, 50)
+        player.dp = pkmath.add(player.dp, pkmath.v2(0, 50))
     if user_input.move_left:
-        player.dp = pkmath.v2(-50, 0)
+        player.dp = pkmath.add(player.dp, pkmath.v2(-50, 0))
     if user_input.move_right:
-        player.dp = pkmath.v2(50, 0)
+        player.dp = pkmath.add(player.dp, pkmath.v2(50, 0))
     player.p = pkmath.add(player.p, pkmath.times(user_input.dt, player.dp))
     player_rect = pygame.Rect(player.p.x, player.p.y, player.w, player.h)
     window_surface.fill((255, 0, 0), player_rect)
